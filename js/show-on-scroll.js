3fc3117c713b310836d2dc99681d9e352ea87c92
Thomas Kolb <kolbt@live.unc.edu>
2019-12-18 14:39:55 -0500

    Revamped the look and feel of the site, more whitespace on the sides smaller text, better resize control when the window size changes

--- js/show-on-scroll.js
+++ js/show-on-scroll.js
@@ -21,21 +21,48 @@ function loop() {
 // Call the loop for the first time
 loop();
 
-// Helper function from: http://stackoverflow.com/a/7557433/274826
+// Revised is-visible function, object should be completely in view
 function isElementInViewport(el) {
-  // special bonus for those using jQuery
   if (typeof jQuery === "function" && el instanceof jQuery) {
     el = el[0];
   }
   var rect = el.getBoundingClientRect();
-  return (
-    (rect.top <= 0
-      && rect.bottom >= 0)
-    ||
-    (rect.bottom >= (window.outerHeight || document.documentElement.clientHeight) &&
-      rect.top <= (window.outerHeight || document.documentElement.clientHeight))
-    ||
-    (rect.top >= 0 &&
-      rect.bottom <= (window.outerHeight || document.documentElement.clientHeight))
-  );
+  var pageHeight = window.innerHeight;
+  var topElement = rect.top;
+  var botElement = rect.bottom;
+  var heightElement = rect.height;
+  return ( ((rect.top - (2 * window.innerHeight / 3)) <= 0) || ((rect.bottom - (window.innerHeight)) <= 0) );
+  // return ( (rect.top >= 0) && (rect.top <= (window.innerHeight - rect.height)) ||
+  //   (rect.top <= (window.innerheight / 2))
+  // );
 }
+
+// // Helper function from: http://stackoverflow.com/a/7557433/274826
+// function isElementInViewport(el) {
+//   // special bonus for those using jQuery
+//   if (typeof jQuery === "function" && el instanceof jQuery) {
+//     el = el[0];
+//   }
+//   var rect = el.getBoundingClientRect();
+//   return ((rect.bottom >= rect.height) && (rect.top <= rect.height));
+// }
+
+// // Helper function from: http://stackoverflow.com/a/7557433/274826
+// function isElementInViewport(el) {
+//   // special bonus for those using jQuery
+//   if (typeof jQuery === "function" && el instanceof jQuery) {
+//     el = el[0];
+//   }
+//   var rect = el.getBoundingClientRect();
+//   return (
+//     (rect.top <= 0
+//       && rect.bottom >= 0)
+//     ||
+//     (rect.bottom >= (window.outerHeight || document.documentElement.clientHeight) &&
+//       rect.top <= (window.outerHeight || document.documentElement.clientHeight))
+//     ||
+//     (rect.top >= 0 &&
+//       rect.bottom <= (window.outerHeight || document.documentElement.clientHeight))
+//   );
+// }
+
