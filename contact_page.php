7897290220f6ac90ecb5a858e1faa2637c605693
Thomas Kolb <kolbt@live.unc.edu>
2019-12-18 16:46:49 -0500

    Can now recieve emails

--- contact_page.php
+++ contact_page.php
@@ -10,13 +10,13 @@
 			
     $body = "From: $fname $lname\n E-Mail: $email\n Message:\n $message";
 				
-    if ($_POST['submit'] && $human == '4') {				 
+    if ($_POST['send'] && $human == '4') {
         if (mail ($to, $subject, $body, $from)) { 
 	    echo '<p>Your message has been sent!</p>';
 	} else { 
 	    echo '<p>Something went wrong, go back and try again!</p>'; 
 	} 
-    } else if ($_POST['submit'] && $human != '4') {
-	echo '<p>You answered the anti-spam question incorrectly!</p>';
+    } else if ($_POST['send'] && $human != '4') {
+        echo '<p>You answered the anti-spam question incorrectly!</p>';
     }
 ?>
